4a743b08663e76ed2de4fd3192042abce7931301
Matt Hellige <matt@immute.net>
2012-04-17 15:38:34 -0500

    Added 'print' and number argument to 'step'.

--- emulator/debugger.c
+++ emulator/debugger.c
@@ -26,6 +26,7 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "dcpu.h"
@@ -39,6 +40,19 @@ static bool matches(char *tok, char *min, char *full) {
   return prefix(min, tok) && prefix(tok, full);
 }
 
+static void dumpram(dcpu *dcpu, u16 addr, int len) {
+  while (len > 0) {
+    u16 base = addr & ~7;
+    dcpu_msg("\n%04x:", base);
+    int pad = addr % 8;
+    dcpu_msg("%*s", 5 * pad, "");
+    do {
+      dcpu_msg(" %04x", dcpu->ram[addr++]);
+      len--;
+    } while (len && addr % 8);
+  }
+  dcpu_msg("\n");
+}
 
 static void dumpheader(void) {
   dcpu_msg(
@@ -75,8 +89,10 @@ bool dcpu_debug(dcpu *dcpu) {
       dcpu_msg(
           "  help, ?: show this message\n"
           "  continue: resume running\n"
-          "  step: execute a single instruction\n"
+          "  step [n]: execute a single instruction (or n instructions)\n"
           "  dump: display the state of the cpu\n"
+          "  print addr [len]: display memory contents in hex\n"
+          "      (addr and len are both hex)\n"
           "  core: dump ram image to core.img\n"
           "  exit, quit: exit emulator\n"
           "unambiguous abbreviations are recognized "
@@ -85,13 +101,47 @@ bool dcpu_debug(dcpu *dcpu) {
     } else if (matches(tok, "con", "continue")) {
       return true;
     } else if (matches(tok, "s", "step")) {
-      dcpu_runterm();
-      dcpu_step(dcpu);
-      dcpu_dbgterm();
-      dumpstate(dcpu);
+      uint32_t steps = 1;
+      tok = strtok(NULL, delim);
+      if (tok) {
+        char *endptr;
+        steps = strtoul(tok, &endptr, 10);
+        if (*endptr) {
+          dcpu_msg("argument to 'step' must be a decimal number\n");
+          continue;
+        }
+      }
+      for (uint32_t i = 0; i < steps; i++) {
+        dcpu_runterm();
+        dcpu_step(dcpu);
+        dcpu_dbgterm();
+        dumpstate(dcpu);
+      }
     } else if (matches(tok, "d", "dump")) {
       dumpheader();
       dumpstate(dcpu);
+    } else if (matches(tok, "p", "print")) {
+      tok = strtok(NULL, delim);
+      if (!tok) {
+        dcpu_msg("print requires an argument\n");
+        continue;
+      }
+      char *endptr;
+      u16 addr = strtoul(tok, &endptr, 16);
+      if (*endptr) {
+        dcpu_msg("addr argument to 'print' must be a hex number: %s\n", endptr);
+        continue;
+      }
+      u16 length = 1;
+      tok = strtok(NULL, delim);
+      if (tok) {
+        length = strtoul(tok, &endptr, 16);
+        if (*endptr) {
+          dcpu_msg("len argument to 'print' must be a hex number\n");
+          continue;
+        }
+      }
+      dumpram(dcpu, addr, length);
     } else if (matches(tok, "cor", "core")) {
       dcpu_coredump(dcpu, 0);
       dcpu_msg("core written to core.img\n");
