f7b96afce9035dfded5afa240c62a9abba2fc1a6
Matt Hellige <matt@immute.net>
2012-04-17 13:09:23 -0500

    Bugfix: screen is only 12 rows high.

--- emulator/dcpu.h
+++ emulator/dcpu.h
@@ -46,7 +46,7 @@ typedef uint64_t tstamp_t;
 
 #define DISPLAY_HZ    30
 #define VRAM_ADDR     0x8000
-#define SCR_HEIGHT    16
+#define SCR_HEIGHT    12
 #define SCR_WIDTH     32
 #define KBD_BAUD      4000
 #define KBD_ADDR      0x9000
